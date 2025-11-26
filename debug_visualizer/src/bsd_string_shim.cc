85de55c5dd0fdff524807b279b4dd501e718ee01
buzzcola3 <buzzcola3@gmail.com>
2025-11-26 14:43:10 +0100

    modified:   debug_visualizer/src/bsd_string_shim.cc

--- debug_visualizer/src/bsd_string_shim.cc
+++ debug_visualizer/src/bsd_string_shim.cc
@@ -1,13 +1,16 @@
 #include <cstring>
 
 // Provide BSD strlcpy for dependencies (e.g., libx11) on glibc versions lacking it.
-extern "C" size_t strlcpy(char* dst, const char* src, size_t size) {
-    if (size == 0) {
+extern "C" size_t strlcpy(char *dst, const char *src, size_t size)
+{
+    if (size == 0)
+    {
         return std::strlen(src);
     }
 
     size_t i = 0;
-    for (; i + 1 < size && src[i] != '\0'; ++i) {
+    for (; i + 1 < size && src[i] != '\0'; ++i)
+    {
         dst[i] = src[i];
     }
 
